cc171022a4d13835e0011e0ddb35b075252129f0
doughnut2002 <shekhar888700@gmail.com>
2024-02-05 16:52:04 +0530

    Done

--- macros/get_payment_type_description.sql
+++ macros/get_payment_type_description.sql
@@ -1,16 +1,34 @@
+-- {#
+-- THIS MACRO RETURNS THE DESCRIPTION OF THE payment_type
+-- #}
+
+-- {%   macro get_payment_type_description -%}
+
+--         case((payment_type))
+--             when 1 then 'Credit Card'
+--             when 2 then 'Cash'
+--             when 3 then 'No Charge'
+--             when 4 then 'Dispute'
+--             when 5 then 'unknown'
+--             when 6 then 'Voided Trip'
+--         end
+
+-- {%- endmacro %}
+
 {#
-THIS MACRO RETURNS THE DESCRIPTION OF THE payment_type
+    This macro returns the description of the payment_type 
 #}
 
-{%   macro get_payment_type_description -%}
+{% macro get_payment_type_description(payment_type) -%}
 
-        case((payment_type))
-            when 1 then 'Credit Card'
-            when 2 then 'Cash'
-            when 3 then 'No Charge'
-            when 4 then 'Dispute'
-            when 5 then 'unknown'
-            when 6 then 'Voided Trip'
-        end
+    case {{ dbt.safe_cast("payment_type", api.Column.translate_type("integer")) }}  
+        when 1 then 'Credit card'
+        when 2 then 'Cash'
+        when 3 then 'No charge'
+        when 4 then 'Dispute'
+        when 5 then 'Unknown'
+        when 6 then 'Voided trip'
+        else 'EMPTY'
+    end
 
 {%- endmacro %}
